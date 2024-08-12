852448cf45a93c0a596e317807b3948376655575
Kundan Tripathi <kundantripathi1512@gmail.com>
2024-08-12 18:42:38 +0530

    updated

--- src/components/FriendsList.jsx
+++ src/components/FriendsList.jsx
@@ -4,7 +4,7 @@ import Friend from "./Friend";
 
 const FriendsList = ({friends, selectedFriend, onSelect}) => {
     // const friends = initialFriends;
-    // const [friends, setFriends] = useState([...initialFriends]);
+
     return <ul>
         {friends.map((friend)=>(
             <Friend friend={friend} key={friend.id} selectedFriend={selectedFriend} onSelect={onSelect}/>
